6216a554f69d3000714dacf783e1f156ce11092a
seunghun <tmdgnsdl23@naver.com>
2021-03-31 20:35:19 +0900

    테이블 뷰 기본 구성

---
+++ please/BountyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BountyViewController.swift
+//  please
+//
+//  Created by 오승훈 on 2021/03/31.
+//
+
+import UIKit
+
+class BountyViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
