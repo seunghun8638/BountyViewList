b56da8e4aecc6915369b816a432b05274769fe7e
seunghun <tmdgnsdl23@naver.com>
2021-03-31 20:57:27 +0900

    TableView Protocol

--- please/BountyViewController.swift
+++ please/BountyViewController.swift
@@ -7,10 +7,27 @@
 
 import UIKit
 
-class BountyViewController : UIViewController {
+class BountyViewController : UIViewController, UITableViewDataSource,UITableViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    //UITableVIewDataSource -> 필수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //indexPath -> cell위치를 표시
+        
+        return cell
+        //checkpoint -> 악세사리
+    }
+    //UITableVIewDelegate -> 눌럿을 때 출력창에 나옴
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--->\(indexPath.row)")
     }
 }
