3462b2d43994f9b3dc258088d6eb1eab16730468
seunghun <tmdgnsdl23@naver.com>
2021-04-01 20:36:52 +0900

    Custom Cell Basic and AutoLayout

--- please/BountyViewController.swift
+++ please/BountyViewController.swift
@@ -9,25 +9,57 @@ import UIKit
 
 class BountyViewController : UIViewController, UITableViewDataSource,UITableViewDelegate{
     
+    //커스텀 셀에 들어갈 데이터
+    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+    let bountyList = [33000000,50,44000000,300000000,16000000,8000000,7700000,120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
     //TableVIew-> DataSource와 Delegate를 BountyViewController에 연결시켜 연동되게 하여 화면에 보여지도록 합니다.
     //UITableVIewDataSource -> 필수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return bountyList.count
+        //boutList 갯수 만큼
     }
     //UITableVIewDataSource -> 필수
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //ListCell 사용위해 추가 as-> 캐스팅  옵셔널이 될 수 있으니 캐스팅이 안된경우에는 UITableViewCell 넘기도록
+        //guard 문 ->  Listcell로 캐스팅한 결과 cell이 nil이 아닌 겨우에 밑으로 코드 수행하고 그렇지 않은 경우에 UITableView로 넘김
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else{
+//            return UITableViewCell()
+//        }
+        //상단 가드문과 if문과 동일
+        //차이는 가드문 띄어쓰기가 한번 더 안들어감
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+            cell.imagView.image = img
+            cell.nameLabel.text = nameList[indexPath.row]
+            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+            
+            return cell
+        }
+        else{
+            return UITableViewCell()
+        }
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         //indexPath -> cell위치를 표시
         
-        return cell
         //checkpoint -> 악세사리
     }
     //UITableVIewDelegate -> 눌럿을 때 출력창에 나옴(선택)
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--->\(indexPath.row)")
     }
+}
+ //custom cell
+class ListCell : UITableViewCell {
+    //custom cell 추가 3개
+    @IBOutlet weak  var imagView: UIImageView!
+    @IBOutlet weak  var nameLabel: UILabel!
+    @IBOutlet weak  var bountyLabel: UILabel!
+    
+    
 }
