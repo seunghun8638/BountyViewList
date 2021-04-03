378cfbad9fff106e252cd67c3f5f0f6ea7fdd19f
seunghun <tmdgnsdl23@naver.com>
2021-04-03 22:38:25 +0900

    Additional Struct

--- please/BountyViewController.swift
+++ please/BountyViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class BountyViewController : UIViewController, UITableViewDataSource,UITableViewDelegate{
+ 
+    //하단 bountyinfo 이용
+    let bountyInfoList : [BountyInfo] = [
+        BountyInfo(name: "brook",bounty: 3300000),
+        BountyInfo(name: "chopper",bounty: 500),
+        BountyInfo(name: "franky",bounty: 5500000),
+        BountyInfo(name: "luffy",bounty: 30000000),
+        BountyInfo(name: "nami",bounty: 1600000),
+        BountyInfo(name: "robin",bounty: 8000000),
+        BountyInfo(name: "sanji",bounty: 7700000),
+        BountyInfo(name: "zoro",bounty: 12000000)
+        
+    ]
     
-    //커스텀 셀에 들어갈 데이터
-    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
-    let bountyList = [33000000,50,44000000,300000000,16000000,8000000,7700000,120000000]
+//    //커스텀 셀에 들어갈 데이터
+//    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+//    let bountyList = [33000000,50,44000000,300000000,16000000,8000000,7700000,120000000]
 
     //기존의 있는 함수여서 override하여 변경
     //seque보내기 직전에 준비 할때 보낼껄 작성하면 된다.
@@ -24,9 +37,14 @@ class BountyViewController : UIViewController, UITableViewDataSource,UITableView
             //그래서 몇번 째 인지를 보내야되니.
             //sender가 위에 any? 옵셔널인데 그걸 int(몇번째를 알아야되기 땜에) 다운 캐스팅
             if let index = sender as? Int{
+                
+                let bountyInfo = bountyInfoList[index]
+//                vc?.name = bountyInfo.name
+//                vc?.bounty = bountyInfo.bounty
+                    vc?.bountyInfo = bountyInfo
                 //있으니깐 nameList에 몇번쨰이다라고 보여줌
-                vc?.name = nameList[index]
-                vc?.bounty = bountyList[index]
+//                vc?.name = nameList[index]
+//                vc?.bounty = bountyList[index]
             }
         }
     }//perfomSegue를 보내기 직전에 준비한다.
@@ -41,7 +59,8 @@ class BountyViewController : UIViewController, UITableViewDataSource,UITableView
     //TableVIew-> DataSource와 Delegate를 BountyViewController에 연결시켜 연동되게 하여 화면에 보여지도록 합니다.
     //UITableVIewDataSource -> 필수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nameList.count
+        return bountyInfoList.count
+//        return nameList.count
         //boutList 갯수 만큼
     }
     //UITableVIewDataSource -> 필수
@@ -54,10 +73,17 @@ class BountyViewController : UIViewController, UITableViewDataSource,UITableView
         //상단 가드문과 if문과 동일
         //차이는 가드문 띄어쓰기가 한번 더 안들어감
         if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
-            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
-            cell.imagView.image = img
-            cell.nameLabel.text = nameList[indexPath.row]
-            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+            
+            //bounyInfo를 이용
+            let bountyInfo = bountyInfoList[indexPath.row]
+            cell.imagView.image = bountyInfo.image
+            cell.nameLabel.text = bountyInfo.name
+            cell.bountyLabel.text = "\(bountyInfo.bounty)"
+            
+//            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//            cell.imagView.image = img
+//            cell.nameLabel.text = nameList[indexPath.row]
+//            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
             
             return cell
         }
@@ -75,7 +101,8 @@ class BountyViewController : UIViewController, UITableViewDataSource,UITableView
         //눌렀을 때 연동(세그웨이 : showDetail)
         // 수행해라 performseque
         //showDetail를 이용하여 넘기면 sender-> 넘길것 따로 없으면 nil
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row) //indexPath.row -> 몇번 쨰 셀이 클릭 되는지 알 수 있다. 그래서 indexPath.row 데이터를 보냄
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+        //indexPath.row -> 몇번 쨰 셀이 클릭 되는지 알 수 있다. 그래서 indexPath.row 데이터를 보냄
         //sender를 이용해서 정보를 보낼 수 있다.
         //정보를 보내는 시점에 그 정보를 가지고 위로 올라간다.
     }
@@ -89,3 +116,28 @@ class ListCell : UITableViewCell {
     
     
 }
+//리펙터링 /,MVVM찾기
+//Model -> 현상금에 대한 정보를
+//BountInfo 만들기(정보를 하나로 만들기)
+struct BountyInfo {
+    let name : String
+    let bounty : Int
+    
+    
+    var image : UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name : String , bounty : Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
+
+//View -> 현상금 관련 정보를 보여줌
+//ListCell -> 필요한 정보를 ViewModel한테서 받아야겠다.
+//ListCell은 ViewModel로 부터 받은 정보를 뷰 업데이트
+
+//ViewModel
+//bountyViewModel 만들기 - View에서 필요한 메서드 만들기
+//Model 가지고 있기 ->bounty info 를 가지고 있어야 한다.
--- please/DetailViewController.swift
+++ please/DetailViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class DetailViewController : UIViewController {
     
+    //리펙터링 /,MVVM찾기
+    //Model -> 현상금에 대한 정보를
+    //BountInfo 만들기(정보를 하나로 만들기)
+    
+    //View
+    //ImageView , nameLavbel,bountyLable
+    //View들은 ViewModel를 통해서 구성되기?
+    
+    //ViewModel
+    //DetailViewModel
+    //뷰레이어에서 필요한 메서드 만들기
+    //모델 가지고 있기(bounty Info)
+    
     
     @IBOutlet weak  var imagView: UIImageView!
     @IBOutlet weak  var nameLabel: UILabel!
     @IBOutlet weak  var bountyLabel: UILabel!
     
-    var name : String?
-    var bounty : Int?
+//    var name : String?
+//    var bounty : Int?
+    var bountyInfo : BountyInfo?
     
     //정보를 받아 Detailviewcontroller 수행 하고 updateUI 실행
     override func viewDidLoad() {
@@ -27,11 +41,17 @@ class DetailViewController : UIViewController {
     //뷰컨트롤러에서 넘어올때 데이터가 가지고 있다면 Ui컴포넌트에 넣을 수 있다.
     //받아온 정보를 받아 정리하여 updateUI를 실행
     func updateUI() {
-        if let name = self.name, let bounty = self.bounty{
-            imagView.image = UIImage(named: "\(name).jpg")
-            nameLabel.text = name
-            bountyLabel.text = "\(bounty)"
+        if let bountyInfo = self.bountyInfo {
+            imagView.image = bountyInfo.image
+            nameLabel.text = bountyInfo.name
+            bountyLabel.text = "\(bountyInfo.bounty)"
         }
+        
+//        if let name = self.name, let bounty = self.bounty{
+//            imagView.image = UIImage(named: "\(name).jpg")
+//            nameLabel.text = name
+//            bountyLabel.text = "\(bounty)"
+//        }
        
         
     }
