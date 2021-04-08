44799a491e4880babf32704758e784061df960b3
seunghun <tmdgnsdl23@naver.com>
2021-04-08 20:36:00 +0900

    Animation constant / View controll

--- please/DetailViewController.swift
+++ please/DetailViewController.swift
@@ -27,7 +27,12 @@ class DetailViewController : UIViewController {
     @IBOutlet weak  var nameLabel: UILabel!
     @IBOutlet weak  var bountyLabel: UILabel!
     
-//    var name : String?
+    //animation 사용 할 곳
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+    
+  
+    //    var name : String?
 //    var bounty : Int?
 //    var bountyInfo : BountyInfo?
     
@@ -37,8 +42,66 @@ class DetailViewController : UIViewController {
         super.viewDidLoad()
         // 보이기 바로 직전 호출
         updateUI()
+        prepareAnimation()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //view didLoad시점에는 가운데 viewDidAppear시점에는 오른쪽
+        showAnimation()
+    }
+    //animate 초기값 설정
+    private func prepareAnimation() {
+//        //x축 위치 변경 , view 기본 width만큼 이동 -> Constant를 이용
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)//namelabel 변경
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)//bountylabel 변경
+        //우측에 있고 3배로 큰 상태 180도 돌아간 상태
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+        
+    }
+    private func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+//        //update된 내용을 0.3초동안
+//        UIView.animate(withDuration: 0.3){
+//            self.view.layoutIfNeeded()
+//        }
+        
+//        //자세한 내용
+//        UIView.animate(
+//            withDuration: 0.3, //-> 진행될건지
+//            delay: 0.1, //-> 진행 딜레이
+//            options: .curveEaseIn, //-> 애니메이션에대한 옵션
+//            animations: {
+//            self.view.layoutIfNeeded() //-> 실제 애니메이션 하는 곳
+//        }, completion: nil) //-> 애니메이션이 끝나고 몰 할것인가
+//
+//        //좀더 애니메이션 주기 + 글자 반동 주기
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+//            self.view.layoutIfNeeded() //layoutIfNeed() -> 레이아웃을 다시 할 필요가 있다면 다시 해라 애니메이션이 동작
+//        }, completion: nil)
+//
+//        //ImagView animation 추가
+//        UIView.transition(with: imagView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+        //view자체를 이용
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1//layoutIfNeed() -> 레이아웃을 다시 할 필요가 있다면 다시 해라 애니메이션이 동작
+                }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1//layoutIfNeed() -> 레이아웃을 다시 할 필요가 있다면 다시 해라 애니메이션이 동작
+                }, completion: nil)
+        
+        UIView.transition(with: imagView, duration: 0.4, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+    }
+        
     //뷰컨트롤러에서 넘어올때 데이터가 가지고 있다면 Ui컴포넌트에 넣을 수 있다.
     //받아온 정보를 받아 정리하여 updateUI를 실행
     func updateUI() {
